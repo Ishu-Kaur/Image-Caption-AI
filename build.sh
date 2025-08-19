04dab36924b3e5bf35052dedc941881bd295cd7a
Ishu Kaur <theishukaur@gmail.com>
2025-08-19 19:24:11 +0530

    Configure build.sh to download models from release

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
+set -e
 
-# Exit on error
-set -o errexit
-
-# Install dependencies
+# Install all the python packages
+echo "--- Installing dependencies ---"
 pip install -r requirements.txt
 
-# Download model files
-echo "--- Downloading models ---"
-wget -O encoder-model.pth "YOUR_DIRECT_DOWNLOAD_LINK_FOR_ENCODER"
-wget -O decoder-model.pth "YOUR_DIRECT_DOWNLOAD_LINK_FOR_DECODER"
-wget -O vocab.pkl "YOUR_DIRECT_DOWNLOAD_LINK_FOR_VOCAB"
-echo "--- Models downloaded successfully ---"
+# Download the large model files from the GitHub Release
+echo "--- Downloading model files ---"
+wget -O decoder-model.pth "https://github.com/Ishu-Kaur/Image-Caption-AI/releases/download/v1.0.0/decoder-model.pth"
+wget -O encoder-model.pth "https://github.com/Ishu-Kaur/Image-Caption-AI/releases/download/v1.0.0/encoder-model.pth"
+wget -O vocab.pkl "https://github.com/Ishu-Kaur/Image-Caption-AI/releases/download/v1.0.0/vocab.pkl"
+
+echo "--- All files downloaded successfully ---"
